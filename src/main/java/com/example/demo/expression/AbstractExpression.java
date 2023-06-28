8fc8a64aab376a003bd63e1b51857377b4232b2f
Rodrigues-Kevin <rodrigues.kevin.pro123@gmail.com>
2023-06-28 15:26:42 +0200

    Amélioration du code

---
+++ src/main/java/com/example/demo/expression/AbstractExpression.java
@@ -0,0 +1,4 @@
+package com.example.demo.expression;
+
+public abstract class AbstractExpression {
+}
---
+++ src/main/java/com/example/demo/expression/LoopExpression.java
@@ -0,0 +1,9 @@
+package com.example.demo.expression;
+
+import java.util.List;
+
+public class LoopExpression {
+    private Class buisnessObject;
+    private String iterator;
+    private List<AbstractExpression> ownedExpression;
+}
